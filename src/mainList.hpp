7a151bab2f935606a66803252435c5fb2502cb65
Mariano Perez <perezmariano7401@gmail.com>
2021-12-23 01:09:05 -0300

    moved mainList class to separate file

---
+++ src/mainList.hpp
@@ -0,0 +1,24 @@
+#ifndef MAINLIST_HPP
+#define MAINLIST_HPP
+
+#include <wx/listctrl.h>
+
+class MainList : public wxListView
+{
+public:
+	MainList(wxWindow *parent,
+			wxWindowID winid = wxID_ANY,
+			const wxPoint& pos = wxDefaultPosition,
+			const wxSize& size = wxDefaultSize,
+			long style = wxLC_REPORT
+			)
+		: wxListView(parent, winid, pos, size, style)
+	{
+		this->AppendColumn("ID", wxLIST_FORMAT_CENTER, 100);
+		this->AppendColumn("Name", wxLIST_FORMAT_CENTER, 500);
+		this->AppendColumn("Stock", wxLIST_FORMAT_CENTER, 100);
+		this->AppendColumn("Price", wxLIST_FORMAT_CENTER, 100);
+	}
+};
+
+#endif
