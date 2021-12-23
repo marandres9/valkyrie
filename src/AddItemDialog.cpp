cae85341d95600567798eba2d230bff9fa432dc5
Mariano Perez <perezmariano7401@gmail.com>
2021-12-23 12:29:01 -0300

    moved AddItemDialog class to separate file

---
+++ src/AddItemDialog.cpp
@@ -0,0 +1,75 @@
+#include "AddItemDialog.hpp"
+
+AddItemDialog::AddItemDialog(wxWindow *parent,
+                            wxWindowID id,
+                            const wxPoint &pos,
+                            const wxSize &size)
+    : wxDialog(parent, id, "Add Item", pos, size)
+{
+    wxGridSizer *listSizer = new wxGridSizer(4, wxSize(1,1));
+    // create textboxes and add them to listSizer
+    wxTextCtrl *idBox = new wxTextCtrl(this, wxID_ANY, "ID", wxDefaultPosition, wxDefaultSize, wxTE_READONLY | wxTE_CENTER);
+    wxTextCtrl *nameBox = new wxTextCtrl(this, wxID_ANY, "Name", wxDefaultPosition, wxDefaultSize, wxTE_READONLY | wxTE_CENTER);
+    wxTextCtrl *stockBox = new wxTextCtrl(this, wxID_ANY, "Stock", wxDefaultPosition, wxDefaultSize, wxTE_READONLY | wxTE_CENTER);
+    wxTextCtrl *priceBox = new wxTextCtrl(this, wxID_ANY, "Price", wxDefaultPosition, wxDefaultSize, wxTE_READONLY | wxTE_CENTER);
+    listSizer->Add(idBox, 1, wxEXPAND);
+    listSizer->Add(nameBox, 1, wxEXPAND);
+    listSizer->Add(stockBox, 1, wxEXPAND);
+    listSizer->Add(priceBox, 1, wxEXPAND);
+    idEntry = new wxTextCtrl(this, wxID_ANY);
+    nameEntry = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize);
+    stockEntry = new wxTextCtrl(this, wxID_ANY);
+    priceEntry = new wxTextCtrl(this, wxID_ANY);
+    // seteo el largo maximo para la celda del nombre
+    nameEntry->SetMaxLength(MAX_NAME_LENGTH);
+    listSizer->Add(idEntry, 1, wxEXPAND);
+    listSizer->Add(nameEntry, 1, wxEXPAND);
+    listSizer->Add(stockEntry, 1, wxEXPAND);
+    listSizer->Add(priceEntry, 1, wxEXPAND);
+
+    // crea los botones del cuadro de dialogo
+    wxSizer *buttonSizer = this->CreateButtonSizer(wxCLOSE | wxAPPLY);
+
+    // crea un sizer principal y agrega el sizer de las celdas y el de los botones
+    wxBoxSizer *mainSizer = new wxBoxSizer(wxVERTICAL);
+    mainSizer->Add(listSizer, 1, wxEXPAND | wxALL, 5);
+    mainSizer->Add(buttonSizer, 0, wxEXPAND | wxALL, 5);
+    this->SetSizerAndFit(mainSizer);
+}
+
+unsigned int AddItemDialog::getID() const {
+    unsigned int id;
+    sscanf(idEntry->GetLineText(0), "%u", &id);
+
+    return id;
+}
+std::string AddItemDialog::getName() const {
+
+    return nameEntry->GetLineText(0).ToStdString();
+}
+unsigned int AddItemDialog::getStock() const {
+    unsigned int stock;
+    sscanf(stockEntry->GetLineText(0), "%u", &stock);
+
+    return stock;
+}
+float AddItemDialog::getPrice() const {
+    float price;
+    sscanf(priceEntry->GetLineText(0), "%f", &price);
+
+    return price;
+}
+
+Item* AddItemDialog::onOkButton() 
+	{
+		Item* item = createItem();
+
+		unsigned int id = getID();
+		const char *name = getName().c_str();
+		unsigned int stock = getStock();
+		float price = getPrice();
+
+		setItem(item, id, name, stock, price);		
+
+		return item;
+	}
---
+++ src/AddItemDialog.hpp
@@ -0,0 +1,34 @@
+#ifndef ADDITEMDIALOG_HPP
+#define ADDITEMDIALOG_HPP
+
+#include <wx/dialog.h>
+#include <wx/textctrl.h>
+#include <wx/sizer.h>
+
+extern "C" {
+    #include "stockControl/itemList.h"
+}
+
+class AddItemDialog : public wxDialog
+{
+public:
+	AddItemDialog(wxWindow *parent,
+                    wxWindowID id = wxID_ANY,
+                    const wxPoint &pos = wxDefaultPosition,
+                    const wxSize &size = wxDefaultSize);
+
+	unsigned int getID() const;
+	std::string getName() const;
+	unsigned int getStock() const;
+	float getPrice() const;
+	
+	Item* onOkButton();
+
+private:
+	wxTextCtrl *idEntry;
+	wxTextCtrl *nameEntry;
+	wxTextCtrl *stockEntry;
+	wxTextCtrl *priceEntry;
+};
+
+#endif
